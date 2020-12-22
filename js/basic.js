91862a3a81b36230960cd4e74ff9bce50caee59b
nklee <namoo3605@naver.com>
2020-12-22 14:02:07 +0900

    1222

---
+++ js/basic.js
@@ -0,0 +1,34 @@
+let num = 10, num3 = 20;
+
+let result = num + num3;
+console.log("result: "+result+"입니다.");
+
+if(result > 30) {
+    console.log("30보다 크다.");
+}else {
+    document.write("<h1>30보다 작거나 같다.</h1>")
+}
+
+document.getElementById();
+
+
+var str = 'Hello, World'
+str = 25;
+str = null;
+str = 1<2;
+str = function() {
+    console.log('function');
+}
+str = 10 + 20;
+console.log(typeof str, str);
+
+num1 = 10;
+console.log(num1);
+
+var str = "reload"; //str 다시 선언해도 문제 없다
+console.log(str);
+
+let num2 = 20;
+console.log(num2);
+// let num2 = 30; 또 선언하면 syntax오류 한번만 선언가능
+// console.log(num2);
---
+++ js/list.js
@@ -0,0 +1,9 @@
+function MyFunc() {
+    document.write("<ul><li>apple</li><li>banana</li></ul>");
+    let dan = 4;
+    document.w
+    for (let i = 1; i <= 10; i++) {
+        document.write('<tr>'+)
+    }
+
+}
